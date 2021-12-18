6169676cfee9fad59f07b22d992bcdaaecff4dd6
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2021-12-18 18:04:01 +0300

    Hello

---
+++ Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace If_Homework_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                      //If1.
+     
+            //
+            // int firstNumber = Convert.ToInt32(Console.ReadLine());
+            // if (firstNumber > 0)
+            // {
+            //     firstNumber = firstNumber + 1;
+            //     
+            // } 
+            // Console.WriteLine(firstNumber);
+            // Console.ReadKey(true);
+            //
+
+            //If 2.
+
+            // int secondNumber = Convert.ToInt32(Console.ReadLine());
+            // if ( secondNumber >0)
+            // {
+            //     secondNumber = secondNumber + 1;
+            //     secondNumber = secondNumber - 2;
+            //     
+            // }
+            // Console.WriteLine(secondNumber);
+            //
+
+
+           // If 3.
+          //
+          //  int integerNumber = Convert.ToInt32(Console.ReadLine());
+          // // integerNumber = 10;
+          //  if (integerNumber > 0)
+          //  {
+          //      integerNumber = integerNumber + 1;
+          //      
+          //  }else if (integerNumber < 0)
+          //  {
+          //      integerNumber -= 2;
+          //  }
+          // else if (integerNumber == 0)
+          //  {
+          //      integerNumber = 10;
+          //      
+          //  }
+          //  Console.WriteLine(integerNumber);
+    
+          //If 4.
+//?          //
+          // int number1 = Convert.ToInt32(Console.ReadLine());
+          // int number2 = Convert.ToInt32(Console.ReadLine());
+          // int number3 = Convert.ToInt32(Console.ReadLine());
+          //
+          //
+          //
+           
+          //If 6
+
+        
+
+          // int firstNumber = Convert.ToInt32(Console.ReadLine());
+          // int secondNumber = Convert.ToInt32(Console.ReadLine());
+          //
+          // if (firstNumber < secondNumber || secondNumber < firstNumber)
+          // {
+          //     Console.WriteLine(firstNumber);
+          // }
+          // else
+          // {
+          //     Console.WriteLine(secondNumber);
+          // }
+
+         //If 8.
+
+         // int thirdNumeral = Convert.ToInt32(Console.ReadLine());
+         // int forthNumber = Convert.ToInt32(Console.ReadLine());
+         //
+         // if (thirdNumeral < forthNumber)
+         // {
+         //     Console.WriteLine(forthNumber);
+         //     Console.WriteLine(thirdNumeral);
+         //     
+         // }
+         // else
+         // {
+         //     Console.WriteLine(thirdNumeral);
+         //     Console.WriteLine(forthNumber);
+         // }
+         
+         //If 9.
+         // double integer1 = 12.30;
+         // double integer2 = 12.05;
+         // double integer3 = 0;
+         // if (integer1 > integer2)
+         // {
+         //     integer1 = integer3;
+         //     integer2 = integer1;
+         //     integer3= integer2;
+         //     Console.WriteLine($"integer1:{integer1},integer2:{integer2} ");
+         // }
+         // else
+         // {
+         //     Console.WriteLine($"integer2:{integer2}, integer1:{integer1}");
+         // }
+         //
+         //
+          
+         
+         //If 10.
+
+          // int numeral = Convert.ToInt32(Console.ReadLine());
+          // int numeral2 = Convert.ToInt32(Console.ReadLine());
+          // int numeral3 = 0;
+          //
+          // if (numeral != numeral2)
+          // {
+          //     numeral = numeral + numeral2;
+          //     Console.WriteLine($"numeral:{numeral}, numeral2:{numeral2}");
+          // }else if (numeral == numeral2)
+          // { 
+          //     numeral = numeral3;
+          //    numeral2 = numeral3;
+          //     Console.WriteLine($"numeral:{numeral}, numeral2:{numeral2}");
+          // }
+
+
+         // int numeral1 = Convert.ToInt32(Console.ReadLine());
+         // int numeral2 = Convert.ToInt32(Console.ReadLine());
+         // int numeral3 = 0;
+         // int numeral4 = 30;
+         //
+         // if (numeral1 != 30 & numeral2 != 30)
+         // {
+         //     numeral1 += numeral4;
+         //     numeral2 += numeral4;
+         //     Console.WriteLine($"numeral1:{numeral1}, numeral2:{numeral2}");
+         // }
+         // else if (numeral1 == numeral4 & numeral2 == numeral4)
+         // {
+         //     Console.WriteLine($"Равны:{numeral1==numeral3}, {numeral2 == numeral3}");
+         // }
+         //
+         //
+        
+         //If 12.
+
+         // int number = Convert.ToInt32(Console.ReadLine());
+         // int number3 = Convert.ToInt32(Console.ReadLine());
+         // int number4 = Convert.ToInt32(Console.ReadLine());
+         //
+         // if (number > number3)
+         // {
+         //     Console.WriteLine(number3);
+         //     if (number4 > number3)
+         //     {
+         //         Console.WriteLine(number3);
+         //     }
+         //     
+         //     
+         //     
+         //     
+         // } 
+
+
+        }
+    }
+}
